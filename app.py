4e71382807f91b70aaf0795a7227da88a54c2c98
rohant25 <learn.rohan.tandalekar@gmail.com>
2024-05-25 21:28:14 +0530

    final merge

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import methods
 
 print(methods.add(5,5))
-<<<<<<< HEAD
-print("Merge conflict in master branch")
-=======
-print("Merge conflict in master branch")
->>>>>>> second-branch
+print("Merge conflict in second branch")
